f3cb24ab1a2c24956e153174ef3fc72acdab5b3f
Sloan <sloangwaltney2@gmail.com>
2021-03-02 12:56:08 -0500

    user ever listeners

---
+++ src/services/users/user.event-listener.js
@@ -0,0 +1,12 @@
+const Email = require('email-templates');
+const path = require('path');
+module.exports = function (app) {
+  app.service('users').on('created', async user => {
+    try {
+      const email = new Email({ message: { from: 'account@account.com', to: user.email }, transport: { jsonTransport: true } });
+      await email.send({ template: path.resolve(__dirname, '../../emails/welcome'), locals: { user } });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
