e910861f5896fc16116d1638ec0a86459847777b
Sloan <sloangwaltney2@gmail.com>
2021-03-02 18:17:42 -0500

    user password service init

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const email = require('./email/email.service.js');
+const userPassword = require('./user-password/user-password.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(email);
+  app.configure(userPassword);
 };
