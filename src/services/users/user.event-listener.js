0d263ecc7f562ae4f31e0053633d72b75814e010
Sloan <sloangwaltney2@gmail.com>
2021-03-02 13:10:56 -0500

    changed so that config is used for the address

--- src/services/users/user.event-listener.js
+++ src/services/users/user.event-listener.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = function (app) {
   app.service('users').on('created', async user => {
     try {
-      const email = new Email({ message: { from: 'account@account.com', to: user.email }, transport: { jsonTransport: true } });
+      const email = new Email({ message: { from: app.get('email').accountsEmail, to: user.email }, transport: { jsonTransport: true } });
       await email.send({ template: path.resolve(__dirname, '../../emails/welcome'), locals: { user } });
     } catch (e) {
       console.log(e);
