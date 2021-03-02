4bdb374a7f1b44d32bf0fb4f22cba06efb680cef
Sloan <sloangwaltney2@gmail.com>
2021-03-02 18:15:39 -0500

    put email on hooks for the moment

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,10 +1,10 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const uuid = require('uuid');
-
+const path = require('path');
 const {
   hashPassword, protect
 } = require('@feathersjs/authentication-local').hooks;
-
+const Email = require('email-templates');
 const setContext = require('../../hooks/setContext');
 
 module.exports = {
@@ -29,7 +29,17 @@ module.exports = {
     ],
     find: [],
     get: [],
-    create: [],
+    create: [
+      // I think this should be on event listeners, however, docs have it like this. I will revist later
+      async ctx => {
+        try {
+          const email = new Email({ message: { from: ctx.app.get('email').accountsEmail, to: ctx.data.email }, transport: { jsonTransport: true } });
+          await email.send({ template: path.resolve(__dirname, '../../emails/welcome'), locals: { user: ctx.data } });
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    ],
     update: [],
     patch: [],
     remove: []
