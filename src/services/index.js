259290978da97c59dac47fcb8187f3343a88a23f
Sloan <sloangwaltney2@gmail.com>
2021-03-02 17:53:01 -0500

    added email service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const users = require('./users/users.service.js');
+const email = require('./email/email.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
+  app.configure(email);
 };
